52bb0d54b7355bd7e1de1bd12206bdad80b3c83a
Samuel Denty <samdenty99@outlook.com>
2017-05-21 20:07:16 +0100

    Cloned from Wi-PWN

--- js/stations.js
+++ js/stations.js
@@ -6,7 +6,7 @@ var scanStatus = getE("spinner-container");
 var clientNames = getE('clientNames');
 var nameListTable = getE('nameList');
 var res;
-
+var edit = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUAgMAAADw5/WeAAAADFBMVEUAAAAAAAAAAAAAAAA16TeWAAAABHRSTlMB/phhN1gO+QAAADtJREFUeF5jQAM8YFIVTIo6AAkm1gIgyenYABIOAAlPjQAJhyaAhEPBwmFowrxgYU6wMGcEiOTbgGYPAPKdCT6Ht/q3AAAAAElFTkSuQmCC'
 function compare(a, b) {
     if (a.p > b.p) return -1;
     if (a.p < b.p) return 1;
@@ -35,18 +35,20 @@ function getResults() {
         clientsFound.innerHTML = '('+res.clients.length+' clients found)';
 
         var tr = '';
-        if (res.clients.length > 0) tr += '<tr><th>Pkts</th><th>Vendor</th><th>Name</th><th>MAC</th><th>AP</th><th>Select</th></tr>';
+        if (res.clients.length > 0) tr += '<tr><th>Name</th><th>Client info</th><th>Pkts</th><th style="padding-left: 40px"></th></tr>';
 
         for (var i = 0; i < res.clients.length; i++) {
 
             if (res.clients[i].s == 1) tr += '<tr class="selected">';
             else tr += '<tr>';
-            tr += '<td>' + res.clients[i].p + '</td>';
-            tr += '<td>' + res.clients[i].v + '</td>';
-            tr += '<td>' + res.clients[i].n + ' <a onclick="changeName(' + res.clients[i].i + ')">edit</a></td>';
-            tr += '<td>' + res.clients[i].m + '</td>';
-            tr += '<td>' + res.clients[i].a + '</td>';
-
+            if (res.clients[i].n == null) {
+                tr += '<td onclick="changeName(' + res.clients[i].i + ')"><a><b>' + res.clients[i].n + '</b></a></td>';
+            } else {
+                tr += '<td onclick="changeName(' + res.clients[i].i + ')"><a>' + res.clients[i].v + '</a></td>';
+            }
+            tr += '<td onclick="changeName(' + res.clients[i].i + ')"><a><b>' + res.clients[i].n + '</b><br>' + res.clients[i].v + '</a></td>';
+            tr += '<td onclick="select(' + res.clients[i].i + ')"><b>' + res.clients[i].m + '</b><br>' + res.clients[i].a + '</td>';
+            tr += '<td onclick="select(' + res.clients[i].i + ')">' + res.clients[i].p + '</td>';
             if (res.clients[i].s == 1) tr += '<td onclick="select(' + res.clients[i].i + ')"><input type="checkbox" name="check' + res.clients[i].i + '" id="check' + res.clients[i].i + '" value="false" checked><label class="checkbox no-events" for="check' + res.clients[i].i + '"></label></td>';
             else tr += '<td onclick="select(' + res.clients[i].i + ')"><input type="checkbox" name="check' + res.clients[i].i + '" id="check' + res.clients[i].i + '" value="false"><label class="checkbox no-events" for="check' + res.clients[i].i + '"></label></td>';
 
@@ -54,17 +56,15 @@ function getResults() {
         }
         table.innerHTML = tr;
 
-        clientNames.innerHTML = res.nameList.length + "/50";
+        clientNames.innerHTML = "("+res.nameList.length + "/50)";
 
-        var tr = '<tr><th>MAC</th><th>Name</th><th>X</th><th>Add</th></tr>';
+        var tr = '<tr><th>Name</th><th>Action</th></tr>';
 
         for (var i = 0; i < res.nameList.length; i++) {
 
             tr += '<tr>';
-            tr += '<td>' + res.nameList[i].m + '</td>';
-            tr += '<td>' + res.nameList[i].n + ' <a onclick="changeName(' + i + ')">edit</a></td>';
-            tr += '<td><button class="marginNull button-warn" onclick="deleteName(' + i + ')">x</button></td>';
-            tr += '<td><button class="marginNull button-primary" onclick="add(' + i + ')">add</button></td>';
+            tr += '<td><b>' + res.nameList[i].n + '</b><br>' + res.nameList[i].m + '</td>';
+            tr += '<td><div class="edit delete" onclick="deleteName(' + i + ')">&times;</div><div class="edit add" onclick="add(' + i + ')">+</div><div class="edit" onclick="changeName(' + i + ')"><img src="'+edit+'"></div></td>';
             tr += '</tr>';
         }
 
@@ -118,7 +118,7 @@ function changeName(id) {
     if (newName != null) {
         getResponse("editNameList.json?id=" + id + "&name=" + newName, function(responseText) {
             if (responseText == "true") getResults();
-            else showMessage("ERROR: Bad response 'editNameList.json'");
+            else document.getElementById("mytext").value = "My value";
         });
     }
 }
